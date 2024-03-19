28b87b8b2fae13f79496f75809d473752479ed2b
sd22039 <buri0901uni0701@icloud.com>
2024-03-20 01:31:40 +0900

    a minor change

--- AI/include/AIBase.hpp
+++ AI/include/AIBase.hpp
@@ -2,14 +2,18 @@
 #define AIBASE_HPP
 
 #include <cstdint>
+#include <functional>
 
 #include "Othello.hpp"
 
 class AIBase{
     private:
         OthelloBoard board;
+        std::function<int(uint64_t, uint64_t)> eval;
+
+    protected:
+        AIBase(std::function<int(uint64_t, uint64_t)> eval);
     public:
-        AIBase();
         void setBoard(uint64_t blackPieces, uint64_t whitePieces);
         virtual uint64_t getMove(Color color);
 };
